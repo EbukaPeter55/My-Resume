ad6add5861e73eec6d48f206c823e6b444303391
ebuka <peterdbrainy5@gmail.com>
2022-05-14 17:06:13 +0100

    create a two column grid with bootstrap, add a profile image and card at the left side

--- src/pages/Resume.component.jsx
+++ src/pages/Resume.component.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import profile from '../assets/images/profile.jpg'
+import './Resume.styles.css';
 
 const Resume = () => {
     return (
-        <div>
-            <h1>Resume</h1>
+        <div className='container-fluid'>
+            <div className='row'>
+                {/* Left section */}
+            <div className='col-md-4 left'>
+            <img className='img-fluid' src={profile} alt="profileImage"/>
+            <div className='card'>
+
+            </div>
+            </div>
+                {/* End of left section */}
+
+            {/* Right section */}
+            <div className='col-md-8'>
+            <h2>Right</h2>
+            </div>
+            </div>
+           
         </div>
     )
 }
