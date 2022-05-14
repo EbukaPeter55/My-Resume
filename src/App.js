e3fa604174a0b5b23596ca35492de00b2756b2c1
ebuka <peterdbrainy5@gmail.com>
2022-05-14 16:20:19 +0100

    Install react-router-dom, add routing to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 // import 'bootstrap/dist/css/bootstrap.min.css'; 
 import './App.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Resume from './pages/Resume.component';
+
 
 function App() {
   return (
     <div>
-      <h1>Resume test</h1>
-      <button className='btn btn-primary' type="button">submit</button>
+      <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<Resume/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
